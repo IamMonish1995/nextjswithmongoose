6f03f518657fe2f61298139daec0cf819c175fb8
monish <monishbarse9@gmail.com>
2023-02-18 15:42:21 +0530

    minor change

--- src/components/gallery/singleimage.js
+++ src/components/gallery/singleimage.js
@@ -4,7 +4,7 @@ import Dialog from "@mui/material/Dialog";
 import Slide from "@mui/material/Slide";
 import CloseIcon from "@mui/icons-material/Close";
 import { Grid } from "@mui/material";
-import "./singleimage.css"
+import "./singleimage.module.css"
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
